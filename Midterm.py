041d49cdbe1c763ace9764893f6f17cd54705d0d
skkaeb <142946459+skkaeb@users.noreply.github.com>
2025-06-26 20:20:46 -0400

    Scaling the data that wasn't encoded through normalization

--- Midterm.py
+++ Midterm.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
+from sklearn.preprocessing import Normalizer
 
 pd.set_option('display.max_columns', None)
 
@@ -26,6 +27,29 @@ df['Drained_after_socializing'].unique()
 label_encoder = preprocessing.LabelEncoder()
 df['Personality']= label_encoder.fit_transform(df['Personality'])
 df['Personality'].unique()
+
+
+
+# Feature Scaling, scaled every feature that wasn't label encoded:
+# Create a Normalized object from SkLearn:
+scaler = Normalizer()
+
+# Making a list of only the features we want to scale, not the one's we Label Encoded
+features_to_scale = ['Time_spent_Alone', 'Social_event_attendance', 'Going_outside',
+                     'Friends_circle_size', 'Post_frequency']
+
+# Grab the data from those columns and normalize them
+#.fit() learns the structure
+#.transform() applies normalization to each row
+scaled_data = scaler.fit_transform(df[features_to_scale])
+
+# Create a new Dataframe with names of the columns the same so it looks like the original table
+scaled_df = pd.DataFrame(scaled_data, columns=features_to_scale)
+
+#Prints the first 5 rows
+print(scaled_df.head())
+
+
 
 # This shows entire updated/encoded table to ensure it's done correctly
 # Can delete later - it's only for testing
